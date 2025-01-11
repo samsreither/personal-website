9580f2123b07d32d3eafa9fcec5bd00810eecd96
Sam Reither <samsreither@gmail.com>
2025-01-10 18:19:42 -0600

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import Header from '../Header/Header'
+
+function App() {
+    return (
+        <div className="page">
+            <div className="page__content">
+                <Header />
+            </div>
+        </div>
+    )
+}
